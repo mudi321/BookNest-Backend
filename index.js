2d5822ccfd3551df0ea5455c44a1f3fd4e7c3758
Muditha Kalansooriya <67087468+mudi321@users.noreply.github.com>
2024-12-05 15:47:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 // //middleware
 // app.use(express.json());
 // app.use(cors({
-//   origin: ["http://localhost:5173"],
+//   origin: ["http://localhost:5173", , 'https://book-nest-delta.vercel.app'],
 //   credentials: true,
 // }));
 
